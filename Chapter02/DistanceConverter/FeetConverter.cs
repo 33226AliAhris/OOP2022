af8a8935aa6473731e5784797916c479a13fa3b9
33226AliAhris <33226@ojs.ac.jp>
2022-04-26 13:50:30 +0900

    FeetConverterクラスを静的クラス化

--- Chapter02/DistanceConverter/FeetConverter.cs
+++ Chapter02/DistanceConverter/FeetConverter.cs
@@ -5,13 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DistanceConverter {
-    public class FeetConverter {
+    public static class FeetConverter {
 
-        public double FromMeter(double meter) {
+        //メートルからフィートを求める（静的メソッド）
+        public static double FromMeter(double meter) {
             return meter / 0.3048;
         }
 
-        public double ToMeter(double feet) {
+        //フィートからメートルを求める（静的メソッド）
+        public static double ToMeter(double feet) {
             return feet * 0.3048;
         }
     }
