ae365a4ca245515be3ede0beb7ccb2b0cdc1341b
33226AliAhris <33226@ojs.ac.jp>
2022-05-10 14:50:46 +0900

    問題2.1.1完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -7,6 +7,23 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Program {
         static void Main(string[] args) {
+
+            var songs = new List<Song> {
+
+               new Song("A", "B", 100),
+               new Song("C", "D", 120),
+               new Song("E", "F", 140),
+               new Song("G", "H", 160),
+               new Song("I", "J", 180)
+
+            };
+            PrintSongs(songs);
+        }
+
+        private static void PrintSongs(List<Song> songs) {
+
+            //songs[0].ForEach(Console.WriteLine);
+            Console.WriteLine(songs[3]);
         }
     }
 }
---
+++ Chapter02/Exercise/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise {
+    class Song {
+
+        public string Title { get; set; } //歌のタイトル
+        public string ArtistName { get; set;} //アーティスト名
+        public int Length { get; set;} //演奏時間、単位は秒
+
+        //引数コンストラクタ
+        public Song(string title, string artistName, int length) {
+            this.Title = title;
+            this.ArtistName = artistName;
+            this.Length = length;
+        }
+
+    }
+}
