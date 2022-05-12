9e928a85ce4c186a77b9fc571e47708ba88275a6
33226AliAhris <33226@ojs.ac.jp>
2022-05-12 15:55:22 +0900

    2.3while文、店舗別、商品別完成

--- Chapter02/Exercise03/Program.cs
+++ Chapter02/Exercise03/Program.cs
@@ -8,27 +8,34 @@ namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("売上高の表示選択");
-            Console.WriteLine("1.店舗別");
-            Console.WriteLine("2.商品カテゴリー別");
-            Console.Write(">");
-            int select = int.Parse(Console.ReadLine());
-
             var sales = new SalesCounter("sales.csv");
 
-            if (select == 1) {
-                var amountPerStore = sales.GetPerStoreSales();
-                foreach (var obj in amountPerStore) {
-                    Console.WriteLine("{0} {1}", obj.Key, obj.Value);
-                }
-            }
+            while (true) {
 
-            else {
-                var amountPerProduct = sales.GetPerProductSales();
-                foreach (var obj in amountPerProduct) {
-                    Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+                Console.WriteLine("売上高の表示選択");
+                Console.WriteLine("1.店舗別");
+                Console.WriteLine("2.商品カテゴリー別");
+                Console.Write(">");
+                int select = int.Parse(Console.ReadLine());
 
+                if (select == 1) {
+                    var amountPerStore = sales.GetPerStoreSales();
+                    foreach (var obj in amountPerStore) {
+                        Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+                    }
                 }
+
+                else if (select == 2) {
+                    var amountPerStore = sales.GetPerProductSales();
+                    foreach (var obj in amountPerStore) {
+                        Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+                    }
+
+                }else if(select == 999) {
+                    return;
+                }
+
+                Console.WriteLine();
             }
         }
     }
