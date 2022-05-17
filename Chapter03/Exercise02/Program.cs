054946df5157d434bfbab205fc39532b3277bd8a
33226AliAhris <33226@ojs.ac.jp>
2022-05-17 15:57:35 +0900

    問題3.2完成

--- Chapter03/Exercise02/Program.cs
+++ Chapter03/Exercise02/Program.cs
@@ -7,7 +7,56 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+            var names = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
 
+            Exercise2_1(names);
+            Console.WriteLine("------------------");
+
+            Exercise2_2(names);
+            Console.WriteLine("------------------");
+
+            Exercise2_3(names);
+            Console.WriteLine("------------------");
+
+            Exercise2_4(names);
+            Console.WriteLine("------------------");
+        }
+
+        private static void Exercise2_1(List<string> names) {
+            Console.WriteLine("都市名を入力。空行で終了");
+            do {
+                var line = Console.ReadLine(); //入力取り込み
+                if (string.IsNullOrEmpty(line))
+                    break;
+
+                var find = names.FindIndex(s => s == line);
+                Console.WriteLine(find);
+                
+            }while (true); //無限ループ
+        }
+
+        private static void Exercise2_2(List<string> names) {
+            var find = names.Count(s => s.Contains("o"));
+            Console.WriteLine(find);
+
+        }
+
+        private static void Exercise2_3(List<string> names) {
+            var find = names.Where(s => s.Contains("o")).ToList();
+            foreach (var s in find) {
+                Console.WriteLine(s);
+
+            }
+        }
+
+        private static void Exercise2_4(List<string> names) {
+            var find = names.Where(s => s.StartsWith("B")).Select(s => s.Length);
+            foreach (var s in find) {
+                Console.WriteLine(s);
+
+            }
         }
     }
 }
