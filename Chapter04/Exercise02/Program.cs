d7a06c9cb6ce6363f2f3952c704f7f571a25ff3c
33226AliAhris <33226@ojs.ac.jp>
2022-05-24 15:37:39 +0900

    問題4.2完成

--- Chapter04/Exercise02/Program.cs
+++ Chapter04/Exercise02/Program.cs
@@ -16,6 +16,70 @@ namespace Exercise02 {
 
             //Console.WriteLine(ym);
 
+            //4.2.1
+            var ymCollection = new YearMonth[] {
+
+                new YearMonth(1990,1),
+                new YearMonth(2000,2),
+                new YearMonth(2001,3),
+                new YearMonth(2021,4),
+                new YearMonth(2022,12),
+
+            }; 
+
+            //4.2.2
+            Exercise2_2(ymCollection);
+            Console.WriteLine("------------------");
+
+            //4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("------------------");
+
+            //4.2.5
+            Exercise2_5(ymCollection);
+            Console.WriteLine("------------------");
+
+        }
+
+        //4.2.3
+        //最初に見つかった21世紀のオブジェクトを返す
+        //見つからない場合はnullを返す
+        static YearMonth FindFirst21C(YearMonth[] yms) {
+            foreach (var ym in yms) {
+                if (ym.Is21Century is true) {
+                    return ym;
+
+                }
+            }
+
+            return null;
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                Console.WriteLine(ym);
+            }
+        }
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            if(FindFirst21C(ymCollection) is null) {
+                Console.WriteLine("21世紀のデータはありません");
+            }
+            else {
+                Console.WriteLine(FindFirst21C(ymCollection));
+            }
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                var newList = new YearMonth[] {
+                    ym.AddOneMonth(),
+                    
+                };
+                foreach (var yms in newList) {
+                    Console.WriteLine(yms);
+                }
+            }
         }
     }
 }
