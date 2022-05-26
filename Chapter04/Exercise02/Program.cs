9d3b6ca113ee09d9f44caa15d7834a00c6b4962a
33226AliAhris <33226@ojs.ac.jp>
2022-05-26 15:06:54 +0900

    問題5.1完成

--- Chapter04/Exercise02/Program.cs
+++ Chapter04/Exercise02/Program.cs
@@ -21,7 +21,7 @@ namespace Exercise02 {
 
                 new YearMonth(1990,1),
                 new YearMonth(2000,2),
-                new YearMonth(2001,3),
+                new YearMonth(2016,3),
                 new YearMonth(2021,4),
                 new YearMonth(2022,12),
 
@@ -81,10 +81,25 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_6(YearMonth[] ymCollection) {
-            foreach (var ym in ymCollection.OrderByDescending(ym => ym.Year)) {
+
+            //①月で昇順に出力、②偶数年のみ昇順に出力
+            foreach (var ym in ymCollection.Where(ym => ym.Year % 2 == 0).OrderBy(ym => ym.Month)) {
                 Console.WriteLine(ym);
+
             }
-            
+
+            //③閏年
+            Console.WriteLine("------------------");
+            foreach (var ym in ymCollection.Where(ym => DateTime.IsLeapYear(ym.Year))) {
+                Console.WriteLine(ym.Year);
+
+            }
+
+            //④すべての年から12月に一番近い月を出力
+            Console.WriteLine("------------------");
+            var max = ymCollection.Max(ym => ym.Month);
+                Console.WriteLine(max);
+
         }
 
     }
---
+++ Chapter05/Exercise01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("文字列１を入力：");
+            string input1 = Console.ReadLine();
+            Console.Write("文字列２を入力：");
+            string input2 = Console.ReadLine();
+
+            if(String.Compare(input1,input2,ignoreCase:true) == 0) {
+                Console.WriteLine("等しい");
+            }
+            else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
