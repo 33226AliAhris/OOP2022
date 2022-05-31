db2ac11c054633010be378e8583991668ff71560
33226AliAhris <33226@ojs.ac.jp>
2022-05-31 14:00:00 +0900

    問題5.4修正版

--- Chapter05/Exercise04/Program.cs
+++ Chapter05/Exercise04/Program.cs
@@ -8,13 +8,12 @@ namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            var split = line.Split(';', '=');
 
-            var noveList = String.Format("{0}: {1}", ToJapanese(split[0]), split[1]);
-            var bestWork = String.Format("{0}: {1}", ToJapanese(split[2]), split[3]);
-            var born = String.Format("{0}: {1}", ToJapanese(split[4]), split[5]);
+            foreach (var item in line.Split(';')) {
+                var array = item.Split('=');
+                Console.WriteLine("{0}: {1}", ToJapanese(array[0]), array[1]);
 
-            Console.WriteLine("{0} \n{1} \n{2}", noveList,bestWork,born);
+            }
 
         }
 
