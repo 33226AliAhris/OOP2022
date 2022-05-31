67768144a0be4da0f5a0582e1988c75a5e13719b
33226AliAhris <33226@ojs.ac.jp>
2022-05-31 13:35:35 +0900

    問題5.3完成

---
+++ Chapter05/Exercise03/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            Exercise3_1(text);
+            Console.WriteLine("-----");
+
+            Exercise3_2(text);
+            Console.WriteLine("-----");
+
+            Exercise3_3(text);
+            Console.WriteLine("-----");
+
+            Exercise3_4(text);
+            Console.WriteLine("-----");
+
+            Exercise3_5(text);
+        }
+
+        private static void Exercise3_1(string text) {
+            var contains = text.Count(c => c == ' ');
+            Console.WriteLine(contains);
+
+        }
+
+        private static void Exercise3_2(string text) {
+            var replace = text.Replace("big", "small");
+            Console.WriteLine(replace);
+        }
+
+        private static void Exercise3_3(string text) {
+            var split = text.Split(' ').Length;
+            Console.WriteLine(split);
+        }
+
+        private static void Exercise3_4(string text) {
+            var count = text.Split(' ').Where(s => s.Length <= 4);
+            foreach (var word in count) {
+                Console.WriteLine(word);
+            }
+        }
+
+        private static void Exercise3_5(string text) {
+            var words = text.Split(' ').ToArray();
+          
+            var sb = new StringBuilder(words[0]);
+            sb.Append(words[0]);
+            foreach (var word in words.Skip(1)) { 
+                sb.Append(' ');
+                sb.Append(word);
+            }
+
+            var convert = sb.ToString();
+            Console.WriteLine(convert);
+        }
+    }
+}
