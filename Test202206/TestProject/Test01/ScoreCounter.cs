f3fa8ca57ccdf2378bad5b83a39f1deb2f2dd8d3
33226AliAhris <33226@ojs.ac.jp>
2022-06-14 14:29:53 +0900

    休み時間

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,40 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
 
-
-
-            
         }
-
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
+            var scores = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                var score = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                scores.Add(score);
+            }
+            return scores;
 
-
-
-
-
-
-            
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _score) {
+                if (dict.ContainsKey(sale.Subject))
+                    dict[sale.Subject] += sale.Score;
 
+                else
 
+                    dict[sale.Subject] = sale.Score;
+            }
 
-
-
-            
+            return dict;
         }
     }
 }
