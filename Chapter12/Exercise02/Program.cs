d6989fdc7efca72c8ba12d173e4a9ed66082ad63
33226AliAhris <33226@ojs.ac.jp>
2022-07-12 14:13:14 +0900

    Exercise02完成

--- Chapter12/Exercise02/Program.cs
+++ Chapter12/Exercise02/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -22,7 +24,7 @@ namespace Exercise02 {
             Console.WriteLine();
 
             // これは確認のためのコード 12.2.2
-            //Console.WriteLine(File.ReadAllText("novelist.json"));
+            Console.WriteLine(File.ReadAllText("novelist.json"));
             Console.WriteLine();
         }
 
@@ -40,7 +42,16 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_2(Novelist novelist, string file) {
+            //日付設定
+            var settings = new DataContractJsonSerializerSettings {
+                DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+            };
 
+            //シリアル化
+            using (var stream = new FileStream(file, FileMode.Create, FileAccess.Write)) {
+                var serializer = new DataContractJsonSerializer(novelist.GetType(), settings);
+                serializer.WriteObject(stream, novelist);
+            }
         }
     }
 }
