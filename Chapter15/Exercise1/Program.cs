231e368764fa47233ac84e304ce8af3bbf60fb2c
33226AliAhris <33226@ojs.ac.jp>
2022-10-06 15:57:11 +0900

    15章練習問題途中

--- Chapter15/Exercise1/Program.cs
+++ Chapter15/Exercise1/Program.cs
@@ -31,23 +31,56 @@ namespace Exercise1 {
         }
 
         private static void Exercise1_2() {
-
+            var max = Library.Books.Max(p => p.Price);
+            var select = Library.Books.First(b => b.Price == max);
+            Console.WriteLine(select);
         }
 
         private static void Exercise1_3() {
-
+            var count = Library.Books.GroupBy(g => g.PublishedYear)
+                .Select(g => new {PublishedYear = g.Key, Count = g.Count() })
+                .OrderBy(o => o.PublishedYear);
+            foreach (var item in count) {             
+                Console.WriteLine($"{item.PublishedYear}年 {item.Count}冊");               
+            }
         }
 
         private static void Exercise1_4() {
+            var selected = Library.Books
+                .Select(b => b)
+                .Join(Library.Categories,          
+                      book => book.CategoryId,      
+                      category => category.Id,      
+                      (book, category) => new {
+                          Title = book.Title,
+                          Category = category.Name,
+                          PublishedYear = book.PublishedYear,
+                          Price = book.Price
+                      });
+            foreach (var book in selected
+                                    .OrderByDescending(x => x.PublishedYear)
+                                    .ThenByDescending(x => x.Price)) {
 
+                Console.WriteLine($"{book.PublishedYear}年 {book.Price}円 {book.Title} ({book.Category})");
+            }
         }
 
         private static void Exercise1_5() {
+            var selected = Library.Books
+                .Where(b => b.PublishedYear == 2016)
+                .Join(Library.Categories,           
+                      book => book.CategoryId,      
+                      category => category.Id,      
+                      (book, category) => category.Name)
+                .Distinct();
 
+            foreach (var item in selected) {
+                Console.WriteLine(item);
+            }
         }
 
         private static void Exercise1_6() {
-
+            
         }
 
         private static void Exercise1_7() {
@@ -55,7 +88,17 @@ namespace Exercise1 {
         }
 
         private static void Exercise1_8() {
-
+            var groups = Library.Categories
+                                .GroupJoin(Library.Books,
+                                           c => c.Id,
+                                           b => b.CategoryId,
+                                           (c, books) => new {
+                                               Category = c.Name,
+                                               Count = books.Count()
+                                           });
+            foreach (var item in groups.Where(c => c.Count >= 4)){
+                Console.WriteLine(item.Category);
+            }
         }
     }
 }
