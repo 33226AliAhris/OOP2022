41d636735ff0d61b596f4483a86d6b15c9e1329f
33226AliAhris <33226@ojs.ac.jp>
2022-10-17 15:58:01 +0900

    NumberGame途中

---
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumberGame {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        int rand = new Random().Next(2);
+
+        public MainWindow() {
+            InitializeComponent();
+
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            Button bt = (Button)sender;
+            if (rand.ToString() == bt.Content.ToString()) {
+                numTextBlock.Text = "当たり";
+
+            }
+            else {
+                numTextBlock.Text = "はずれ";
+            }
+        }
+    }
+}
--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -20,6 +20,34 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            seasonTextBlock.Text = "春";
+            seasonComboBox.SelectedIndex = getSeason();
+        }
+
+        private static int getSeason() {
+            var month = int.Parse(DateTime.Now.Month.ToString("00"));
+            var season = 0;
+
+            switch (month) {
+
+                case int n when (n >= 3 && n <= 5):
+                    season = 0;
+                    break;
+
+                case int n when (n >= 6 && n <= 8):
+                    season = 1;
+                    break;
+
+                case int n when (n >= 9 && n <= 11):
+                    season = 2;
+                    break;
+
+                case int n when (n >= 1 && n <= 2):
+                case 12:
+                    season = 3;
+                    break;
+            }
+            return season;
         }
 
         private void checkBox_Unchecked(object sender, RoutedEventArgs e) {
